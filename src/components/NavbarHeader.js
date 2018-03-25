a8b99f9cce11fc310916556a83a80a615add54aa
Sander Huijsen <sander.huijsen@gmail.com>
2018-03-25 17:18:44 +0800

    Navbar header is now clickable.

--- src/components/NavbarHeader.js
+++ src/components/NavbarHeader.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
+import { withRouter } from 'react-router-dom';
 
 const NavbarHeader = () => (
   <Navbar.Header>
     <Navbar.Brand>
-      Raspberry Pi Speedtest
+      <a href="/">Raspberry Pi Speedtest</a>
     </Navbar.Brand>
   </Navbar.Header>
 );
 
-export default NavbarHeader;
+export default withRouter(NavbarHeader);
