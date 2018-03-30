21bbffcd45aa154b643274c2c57b7544c5b344ce
Sander Huijsen <sander.huijsen@gmail.com>
2018-03-30 17:59:49 +0800

    Removed the redux components for Stats (actions, reducer, etc.) since it is now handled by Apollo client.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import sampleReducer from './sampleReducer';
-import statsReducer from './statsReducer';
 import tokenReducer from './tokenReducer';
 import userReducer from './userReducer';
 
 const rootReducer = combineReducers({
   data: sampleReducer,
-  stats: statsReducer,
   token: tokenReducer,
   user: userReducer,
 });
