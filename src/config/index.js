96cbf8d829e4e29072e436b78a5df169aff95e8f
Sander Huijsen <sander.huijsen@gmail.com>
2018-04-01 12:27:15 +0800

    Small change in config.

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
-import { apiEndpoint, clientId, redirectUrl } from './config.json';
+import * as Config from './config.json';
 
-export const API_ENDPOINT = apiEndpoint;
-export const CLIENT_ID = clientId;
-export const REDIRECT_URL = redirectUrl;
+export const API_ENDPOINT = Config.apiEndpoint;
+export const CLIENT_ID = Config.clientId;
+export const REDIRECT_URL = Config.redirectUrl;
