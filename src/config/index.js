67dc03b516451a35c0d637b9eaa6ec0600ff0ca4
Sander Huijsen <sander.huijsen@gmail.com>
2018-04-01 11:25:30 +0800

    Just usee the flipping env variables alright.

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,3 @@
-export let API_ENDPOINT = process.env.PI_API_ENDPOINT;
-export let  CLIENT_ID = process.env.PI_CLIENT_ID;
-export let REDIRECT_URL = process.env.PI_REDIRECT_URL;
-
-try {
-  const config = require('./config-dev');
-  API_ENDPOINT = API_ENDPOINT || config.apiEndpoint;
-  CLIENT_ID = CLIENT_ID || config.clientId;
-  REDIRECT_URL = REDIRECT_URL || config.redirectUrl;
-} catch (e) {}
+export const API_ENDPOINT = process.env.PI_API_ENDPOINT;
+export const CLIENT_ID = process.env.PI_CLIENT_ID;
+export const REDIRECT_URL = process.env.PI_REDIRECT_URL;
