7a51fa39c306b9646d069777bd1f2d0aa9ac0130
Sander Huijsen <sander.huijsen@gmail.com>
2018-04-01 11:18:12 +0800

    Changed how configuration is read.

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,10 @@
-import { apiEndpoint, redirectUrl, clientId } from './config-dev.json';
+export let API_ENDPOINT = process.env.PI_API_ENDPOINT;
+export let  CLIENT_ID = process.env.PI_CLIENT_ID;
+export let REDIRECT_URL = process.env.PI_REDIRECT_URL;
 
-export const API_ENDPOINT = process.env.PI_API_ENDPOINT || apiEndpoint;
-export const CLIENT_ID = process.env.PI_CLIENT_ID || clientId;
-export const REDIRECT_URL = process.env.PI_REDIRECT_URL || redirectUrl;
+try {
+  const config = require('./config-dev');
+  API_ENDPOINT = API_ENDPOINT || config.apiEndpoint;
+  CLIENT_ID = CLIENT_ID || config.clientId;
+  REDIRECT_URL = REDIRECT_URL || config.redirectUrl;
+} catch (e) {}
