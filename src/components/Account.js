3cec271570d4bbd828f1a4d714ced2aaf902c207
Sander Huijsen <sander.huijsen@gmail.com>
2018-04-02 20:39:35 +0800

    Turned Account into class.

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,45 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Col, Grid, Panel, Row } from 'react-bootstrap';
 import AppMenu from '../containers/AppMenu';
 import Danger from '../containers/Danger';
 import Token from '../containers/Token'
 import User from './User';
 
-const Account = () => (
-  <div>
-    <AppMenu />
-    <Grid bsClass="container">
-      <h2>Account</h2>
-      <Row><br/></Row>
-      <Row>
-        <Panel>
-          <Col smOffset={1}>
-            <br/><h3>User</h3><br/>
-          </Col>
-          <User />
-          <br/>
-        </Panel>
-      </Row>
-      <Row>
-        <Panel>
-          <Col smOffset={1}>
-            <br/><h3>Authentication</h3><br/>
-          </Col>
-          <Token />
-          <br/>
-        </Panel>
-      </Row>
-      <Row>
-        <Panel>
-          <Col smOffset={1}>
-            <br/><h3>Danger zone</h3><br/>
-          </Col>
-          <Danger />
-          <Col><br/><br/></Col>
-        </Panel>
-      </Row>
-    </Grid>
-  </div>
-);
+class Account extends Component {
+  render() {
+    return (
+      <div>
+        <AppMenu />
+        <Grid bsClass="container">
+          <h2>Account</h2>
+          <Row><br/></Row>
+          <Row>
+            <Panel>
+              <Col smOffset={1}>
+                <br/><h3>User</h3><br/>
+              </Col>
+              <User />
+              <br/>
+            </Panel>
+          </Row>
+          <Row>
+            <Panel>
+              <Col smOffset={1}>
+                <br/><h3>Authentication</h3><br/>
+              </Col>
+              <Token />
+              <br/>
+            </Panel>
+          </Row>
+          <Row>
+            <Panel>
+              <Col smOffset={1}>
+                <br/><h3>Danger zone</h3><br/>
+              </Col>
+              <Danger />
+              <Col><br/><br/></Col>
+            </Panel>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+}
 
 export default Account;
