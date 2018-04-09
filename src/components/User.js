09462489a918749b811ac8f80f91425a9cb1ef75
Sander Huijsen <sander.huijsen@gmail.com>
2018-04-09 15:02:18 +0800

    Added additional information to user section in Account page by using the "me" endpoint.

--- src/components/User.js
+++ src/components/User.js
@@ -1,34 +1,76 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import propTypes from 'prop-types';
+import { Query } from 'react-apollo';
 import {
-  Col, ControlLabel, Form, FormControl, FormGroup
+  Col, ControlLabel, Form, FormControl, FormGroup, Image
 } from 'react-bootstrap';
+import gql from 'graphql-tag';
+
+const GET_USER_INFO = gql`
+query {
+  me { 
+    email 
+    avatarUrl 
+  }
+}
+`;
 
 class User extends Component {
   render() {
     const { id, fullName } = this.props;
 
     return (
-      <Form horizontal sm={8}>
-        <FormGroup controlId='formHorizontalToken'>
-          <Col componentClass={ControlLabel} sm={2}>
-            ID
-          </Col>
-          <Col sm={6}>
-            <FormControl.Static>{id || 'Your id will show up here'}</FormControl.Static>
-          </Col>
-        </FormGroup>
-        <FormGroup controlId='formHorizontalToken'>
-          <Col componentClass={ControlLabel} sm={2}>
-            Name
-          </Col>
-          <Col sm={6}>
-            <FormControl.Static>{fullName || 'Your id will show up here'}</FormControl.Static>
-          </Col>
-        </FormGroup>
-      </Form>
-    )
+      <Query query={GET_USER_INFO} fetchPolicy="cache-first">
+        {({ loading, error, data }) => {
+          let imageHeader = null;
+          let emailField = null;
+          let me;
+          if (!loading && !error) {
+            me = data.me;
+            console.log(me);
+
+            emailField = <FormGroup controlId='formHorizontalToken'>
+              <Col componentClass={ControlLabel} sm={2}>
+                Email
+              </Col>
+              <Col sm={6}>
+                <FormControl.Static>{me.email || 'Your email address will show up here'}</FormControl.Static>
+              </Col>
+            </FormGroup>;
+
+            imageHeader = <FormGroup controlId='formHorizontalToken'>
+              <Col componentClass={ControlLabel} smOffset={2} style={{ paddingLeft: "15px" }}>
+                <Image src={me.avatarUrl} rounded width={128} height={128}/>
+              </Col>
+            </FormGroup>
+          }
+
+          return (
+            <Form horizontal sm={6}>
+              <FormGroup controlId='formHorizontalToken'>
+                <Col componentClass={ControlLabel} sm={2}>
+                  ID
+                </Col>
+                <Col sm={4}>
+                  <FormControl.Static>{id || 'Your id will show up here'}</FormControl.Static>
+                </Col>
+              </FormGroup>
+              <FormGroup controlId='formHorizontalToken'>
+                <Col componentClass={ControlLabel} sm={2}>
+                  Name
+                </Col>
+                <Col sm={4}>
+                  <FormControl.Static>{fullName || 'Your id will show up here'}</FormControl.Static>
+                </Col>
+              </FormGroup>
+              {emailField}
+              {imageHeader}
+            </Form>
+          );
+        }}
+      </Query>
+    );
   }
 }
 
