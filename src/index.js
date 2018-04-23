7af49ad9a527e15b5093b79cc07b90015c15c255
Sander Huijsen <sander.huijsen@gmail.com>
2018-04-23 11:39:24 +0800

    Fixed typo.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { ApolloLink } from 'apollo-link';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-import { API_ENDPOINT }from './config';
+import { API_ENDPOINT } from './config';
 
 import reducer from './reducers'
 import thunk from 'redux-thunk'
